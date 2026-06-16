a10048ecbf46f273216216f3bb340db132d03683
Mohammad Aslam <aslampaasa420@gmail.com>
2026-06-16 15:06:50 +0530

    Local MCP Server: How Tools work BTS

--- 02-MCP-Implementation/server/src/index.ts
+++ 02-MCP-Implementation/server/src/index.ts
@@ -201,3 +201,15 @@ main().catch((error) => {
  *
  * Note: Similarly we can use MCP in other apps like Cursor App.
  */
+
+/**
+ * What's happening under the hood?
+ * > When you as a question:
+ *   1. The client sends your question to Claude
+ *   2. Claude analyzes the available tools and decides which one(s) to use
+ *   3. The client executes the chosen tool(s) throught the MCP Server
+ *   4. The results are sent back to Claude
+ *   5. Claude formulates a natural language response
+ *   6. The response is displayed to you!
+*/
+
