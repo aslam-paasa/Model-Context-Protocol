d20e87988ff9f668e34d4b68d3494327b43b0597
Mohammad Aslam <aslampaasa420@gmail.com>
2026-06-17 22:44:27 +0530

    MCP Client: Connect to Local Server

--- 02-MCP-Implementation/client/src/index.ts
+++ 02-MCP-Implementation/client/src/index.ts
@@ -1,1 +1,100 @@
-console.log("Welcome to MCP Client")
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js";
+import { Transport } from "@modelcontextprotocol/sdk/shared/transport.js";
+import OpenAI from "openai";
+import { FunctionTool } from "openai/resources/beta.js";
+import readline from "readline/promises";
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+if (!OPENAI_API_KEY) {
+  throw new Error("OPENAI_API_KEY is not set");
+}
+
+class MCPClient {
+  private mcp: Client;
+  private openai: OpenAI;
+
+  private transport:
+    | StdioClientTransport
+    | StreamableHTTPClientTransport
+    | null = null;
+
+  private tools: FunctionTool[] = [];
+
+  constructor() {
+    this.openai = new OpenAI({
+      apiKey: OPENAI_API_KEY,
+    });
+
+    this.mcp = new Client({
+      name: "cg-mcp-client",
+      version: "1.0.0",
+    });
+  }
+
+  async connectToServer(serverScriptPath: string) {
+    try {
+      const isJs = serverScriptPath.endsWith(".js");
+      const isPy = serverScriptPath.endsWith(".py");
+
+      if (!isJs && !isPy) {
+        throw new Error("Server script must be a .js or .py file");
+      }
+
+      const command = isPy
+        ? process.platform === "win32"
+          ? "python"
+          : "python3"
+        : process.execPath;
+
+      this.transport = new StdioClientTransport({
+        command,
+        args: [serverScriptPath],
+      });
+
+      await this.mcp.connect(this.transport as Transport);
+
+      const toolsResult = await this.mcp.listTools();
+      this.tools = toolsResult.tools.map((tool) => {
+        return {
+          type: "function",
+          function: {
+            name: tool.name,
+            parameters: tool.inputSchema,
+            description: tool.description as string,
+          },
+        };
+      });
+
+      console.log(
+        "Connected to server with tools:",
+        this.tools.map((tool) => tool.function.name),
+      );
+    } catch (e) {
+      console.log("Failed to connect to MCP server: ", e);
+      throw e;
+    }
+  }
+}
+
+
+async function main() {
+  if (process.argv.length < 3) {
+    console.log("Usage: node index.ts <path_to_server_script>");
+    return;
+  }
+
+  const mcpClient = new MCPClient();
+
+  try {
+    await mcpClient.connectToServer(process.argv[2]);
+  } catch (e) {
+    console.error("Error:", e);
+    process.exit(1);
+  } finally {
+    process.exit(0);
+  }
+}
+
+main();
